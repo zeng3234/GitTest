b94b0291161975a55ef7d4e82ca344e47f41ab6f
zengguangfu <zengguangfu@kobox.tv>
2015-05-12 17:42:30 +0800

    ssh test

--- AndroidVolley/src/com/android/volley/test/MainActivity.java
+++ AndroidVolley/src/com/android/volley/test/MainActivity.java
@@ -1,6 +1,16 @@
 package com.android.volley.test;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 
-public class MainActivity extends Activity{ }
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    } 
+    
+    
+}
